af3cf6119869c67da5714f85cd8b3eaa20c4d34c
Md Aminul Islam <mdaminulislam2015bd@gmail.com>
2024-02-04 21:09:43 +0600

    Update : Personal Portfolio config file in vite

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        // Specify the output directory for the built files
+        outDir: 'dist',
+
+        // Specify the base path for the project (optional)
+        base: '/',
+
+        // Configure the assets subdirectory
+        assetsDir: 'assets',
+
+        rollupOptions: {
+            // If you have a specific entry point for your application
+            input: {
+                main: 'src/js/main.js',
+            },
+        },
+    },
+
+
+    server: {
+        // Specify the port for the development server
+        port: 3000,
+
+        // Configure proxy settings if needed
+        proxy: {
+            // '/api': 'http://localhost:5000',
+        },
+    },
+
+    // Configure the CSS preprocessor for SCSS
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // Specify the path to your main SCSS file
+                // additionalData: `@import "src/scss/style.scss";`,
+            },
+        },
+    },
+});
